fa1a80b7e516fdf3a65e1742d2c203438f0901b2
moleukert <moleukert@onlinehome.de>
2024-06-09 12:32:53 +0200

    chore: added more instructions

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,6 +1,7 @@
 enum Instruction {
-    // opcode: 0000 0000, does nothing
+    // opcode: 0000 0000, no operation
     Nop,
+    // simple load operations
     // opcode: 00 +(16b register)+ 0001, load next 2 bytes as 16b immidiate into r16
     Ld16im { reg: u8, imm: u16 },
     // opcode: 00 +[16b register]+ 0010, load value of a into memory location pointed by [r16]
@@ -16,6 +17,35 @@ enum Instruction {
     Dec16 { reg: u8 },
     // opcode: 00 +(16b register)+ 0001, adds r16 to hl
     Addhl16 { reg: u8 },
+
+    // opcode 0000 0111, rotate a left
+    Rlca,
+    // opcode 0000 1111, rotate a right
+    Rrca,
+    // opcode 0001 0111, rotate a left (through the carry)
+    Rla,
+    // opcode 0001 1111, rotate a right (through the carry)
+    Rra,
+    // opcode 0010 0111, adjusts a to be a correct bdc interpretation by adding 6 to the respective nibble (4 low or high bits) and adjusting the carry
+    Daa,
+    // opcode 0010 1111, complements a bitwise
+    Cpl,
+    // opcode 0011 0111, sets carry flag
+    Scf,
+    // opcode 0011 1111, complements carry flag
+    Ccf,
+
+    // opcode 0001 1000, jumps relatively using next byte as 8b immidiate
+    Jr { imm: u8 },
+    // opcode 001 +(condition)+ 000, jumps relatively using next byte as 8b immidiate if condition is true
+    Jrcond { cond: u8, imm: u8 },
+    // opcode 0001 0000, stops and enters low power mode, depending on next byte behaviour may vary (TODO)
+    Stop { next: u8 },
+
+    // opcode: 01 +(8b register dest)+(8b register source), loads value of source r8 into destination r8
+    Ld88 { dest: u8, source: u8 },
+    // exception: 0111 0110, loading [hl] into [hl] instead halts (check documentation on halt for more info)
+    Halt,
 
     // opcode: 00 +(8b register)+ 100, increments r8
     Inc8 { reg: u8 },
@@ -60,6 +90,8 @@ enum Instruction {
     // opcode: 1111 1110, adds next byte as 8b immidiate to a without saving (compare)
     Cpim { imm: u8 },
 
+    // return and jump instructions
+
     // prefix instructions: opcode of prefix: 1100 1011
     // opcode: 0000 0 +(8b register), rotate r8 left
     Rlc { reg: u8 },
@@ -84,6 +116,10 @@ enum Instruction {
     Res { index: u8, reg: u8 },
     // opcode 11 +(3b index)+(8b register), set bit at index in r8 to 1
     Set { index: u8, reg: u8 },
+
+    // all the following opcodes are invalid and hard lock cpu by never fetching (just exit in this case)
+    // 1101 0011, 1101 1011, 1101 1101, 1110 0011, 1110 0100, 1110 1011, 1110 1100, 1110 1101, 1111 0100, 1111 1100, 1111 1101
+    Inv,
 }
 
 // six 8bit registers (b, c, d, e, h, l, a)
@@ -91,3 +127,5 @@ enum Instruction {
 // 4 16bit registers (bc, de, hl, sp)
 // organisation: 00 = bc, 01 = de, 10 = hl, 11 = sp (stack pointer)
 // 4 16bit pointers ([bc], [de], [hl+], [hl-]) respectively 00 01 10 11
+
+// 4 different conditions depending on the zero and carry flags: (nz, z, nc, c) respectively 00 01 10 11
